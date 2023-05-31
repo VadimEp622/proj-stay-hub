ccc31c96aa62bbffe95e94480fddc486c85c7579
Vlad Epov <vladep622@gmail.com>
2023-05-31 18:36:51 +0300

    changed car service all names to stay

---
+++ frontend/src/services/stay.service.js
@@ -0,0 +1,64 @@
+
+// import { storageService } from './async-storage.service.js'
+import { httpService } from './http.service.js'
+import { utilService } from './util.service.js'
+import { userService } from './user.service.js'
+
+
+const STORAGE_KEY = 'stay'
+
+export const stayService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyStay,
+    addStayMsg
+}
+window.cs = stayService
+
+
+async function query(filterBy = { name: '', price: 0 }) {
+    return httpService.get(STORAGE_KEY, filterBy)
+}
+
+function getById(stayId) {
+    return httpService.get(`stay/${stayId}`)
+}
+
+async function remove(stayId) {
+    return httpService.delete(`stay/${stayId}`)
+}
+async function save(stay) {
+    let savedStay
+    if (stay._id) {
+        savedStay = await httpService.put(`stay/${stay._id}`, stay)
+
+    } else {
+        savedStay = await httpService.post('stay', stay)
+    }
+    return savedStay
+}
+
+async function addStayMsg(stayId, txt) {
+    const savedMsg = await httpService.post(`Stay/${stayId}/msg`, { txt })
+    return savedMsg
+}
+
+
+function getEmptyStay() {
+    return {
+        name: '',
+        description: '',
+        price: utilService.getRandomIntInclusive(100, 9000),
+        type: "House",
+        imgUrls: [],
+        summary: "Fantastic duplex apartment...",
+        capacity: utilService.getRandomIntInclusive(1, 10),
+    }
+}
+
+
+
+
+
---
+++ frontend/src/services/stay.service.local.js
@@ -0,0 +1,85 @@
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+import { userService } from './user.service.js'
+
+const STORAGE_KEY = 'stay'
+
+export const stayService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyStay,
+    addStayMsg
+}
+window.cs = stayService
+
+
+async function query(filterBy = { name: '', price: 0 }) {
+    var stays = await storageService.query(STORAGE_KEY)
+    if (filterBy.name) {
+        const regex = new RegExp(filterBy.txt, 'i')
+        stays = stays.filter(stay => regex.test(stay.name) || regex.test(stay.description))
+    }
+    if (filterBy.price) {
+        stays = stays.filter(stay => stay.price <= filterBy.price)
+    }
+    return stays
+}
+
+function getById(stayId) {
+    return storageService.get(STORAGE_KEY, stayId)
+}
+
+async function remove(stayId) {
+    // throw new Error('Nope')
+    await storageService.remove(STORAGE_KEY, stayId)
+}
+
+async function save(stay) {
+    var savedStay
+    if (stay._id) {
+        savedStay = await storageService.put(STORAGE_KEY, stay)
+    } else {
+        // Later, owner is set by the backend
+        stay.owner = userService.getLoggedinUser()
+        savedStay = await storageService.post(STORAGE_KEY, stay)
+    }
+    return savedStay
+}
+
+async function addStayMsg(stayId, txt) {
+    // Later, this is all done by the backend
+    const stay = await getById(stayId)
+    if (!stay.msgs) stay.msgs = []
+
+    const msg = {
+        id: utilService.makeId(),
+        by: userService.getLoggedinUser(),
+        txt
+    }
+    stay.msgs.push(msg)
+    await storageService.put(STORAGE_KEY, stay)
+
+    return msg
+}
+
+function getEmptyStay() {
+    return {
+        name: '',
+        description: '',
+        price: utilService.getRandomIntInclusive(100, 9000),
+        type: "House",
+        imgUrls: [],
+        summary: "Fantastic duplex apartment...",
+        capacity: utilService.getRandomIntInclusive(1, 10),
+    }
+}
+
+
+// TEST DATA
+// storageService.post(STORAGE_KEY, {vendor: 'Subali Rahok 2', price: 980}).then(x => console.log(x))
+
+
+
+
