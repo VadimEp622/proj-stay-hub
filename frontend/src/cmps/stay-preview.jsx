30ef8056f1f249c4f5d09aa747976b44328e5b40
Steveb599 <Steveb599@gmail.com>
2023-06-01 10:07:36 +0300

    catching up

--- frontend/src/cmps/stay-preview.jsx
+++ frontend/src/cmps/stay-preview.jsx
@@ -4,10 +4,13 @@ export function StayPreview({ stay }) {
     return (
         <div className="stay-preview" key={stay._id}>
             <img src={stay.imgUrls[0]} alt="stay-image" />
-            <p>{stay.loc.city}, {stay.loc.country}</p>
-            <p>{utilService.formatTime(stay.checkIn)} - {utilService.formatTime(stay.checkOut)}</p>
-            {console.log(stay)}
-            <p>${stay.price.toLocaleString()} night</p>
+            <div className="preview-info">
+                <p>{stay.loc.city}, {stay.loc.country}</p>
+                {console.log(stay)}
+                <p>{utilService.formatTime(stay.checkIn, stay.checkOut)}</p>
+                {console.log(stay)}
+                <p>${stay.price.toLocaleString()} night</p>
+            </div>
         </div>
     )
 }
