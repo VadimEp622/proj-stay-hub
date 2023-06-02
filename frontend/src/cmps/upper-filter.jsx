1831a8bbde09881fa61520d3e22cbaa43bb5774f
Steveb599 <Steveb599@gmail.com>
2023-06-03 00:04:32 +0300

    added overlay and changes to details

--- frontend/src/cmps/upper-filter.jsx
+++ frontend/src/cmps/upper-filter.jsx
@@ -1,5 +1,138 @@
+import React, { useEffect, useRef, useState } from "react";
+import { CarouselImage2 } from "./carousel2";
+
 export function UpperFilter() {
+    const [jpegFiles, setJpegFiles] = useState([]);
+    const windowWidth = useRef(window.innerWidth);
+    const carouselRef = useRef(null);
+
+    useEffect(() => {
+        const handleResize = () => {
+            windowWidth.current = window.innerWidth;
+            carouselRef.current?.reInit();
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [])
+
+    useEffect(() => {
+        const fileNames = [
+            "Beachfront",
+            "Countryside",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Beachfront",
+            "Countryside",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Countryside",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+            "Beachfront",
+            "Countryside",
+            "Lakefront",
+            "Lakefront",
+            "Lakefront",
+        ]
+
+        Promise.all(
+            fileNames.map((fileName) =>
+                import(`../assets/img/airbnb-assets/${fileName}.jpeg`)
+                    .then((module) => ({ fileName, module }))
+                    .catch((error) => {
+                        console.error(`Error loading ${fileName}:`, error);
+                        return null;
+                    })
+            )
+        )
+            .then((files) => {
+                setJpegFiles(files.filter((file) => file !== null));
+            })
+            .catch((err) => {
+                console.error("Error loading JPEG files:", err);
+            });
+    }, []);
+
+    if (jpegFiles.length === 0) {
+        return <div>Loading...</div>;
+    }
+
+
     return (
-        <p>This is upper filter</p>
-    )
+        <section className="label-filter">
+            {/* {jpegFiles.map(({ fileName, module }) => (
+                <div className="upper-filter-icon">
+                    {fileName}
+                    <img
+                        key={fileName}
+                        src={module.default}
+                        alt={fileName}
+                        style={{ width: "24px", height: "24px" }}
+                    />
+                </div> */}
+            {/* <CarouselImage2 imgs={jpegFiles} /> */}
+            {/* ))} */}
+        </section>
+    );
 }
--- frontend/src/pages/stay-details.jsx
+++ frontend/src/pages/stay-details.jsx
@@ -89,10 +89,11 @@ export function StayDetails() {
         return percentage.toFixed(1)
     }
 
-    const reviewsInputs = displayReviewsCriteria()
-    console.log(reviewsInputs)
-    const reviews = stay.reviews.length > 1 ? 'reviews' : 'review'
     if (!stay) return <div>Loading...</div>
+    const reviewsInputs = displayReviewsCriteria()
+    const reviews = stay.reviews.length > 1 ? 'reviews' : 'review'
+    const capitalizedReviewsString = reviews.charAt(0).toUpperCase() + reviews.slice(1)
+
     return <section className="stay-details">
         <section className='stay-review flex'>
             <h1>{stay.name}</h1>
@@ -100,17 +101,19 @@ export function StayDetails() {
                 <section className='info flex'>
                     <SvgHandler svgName={STAR} />
                     <span>{reviewService.getAverageReview(stay)}</span>
-                    <span className='info-review'>{stay.reviews.length} Review</span>
+                    <span className='info-review'>{stay.reviews.length} {reviews}</span>
                     <span className='info-loc'>{stay.loc.city}, {stay.loc.country}</span>
                 </section>
                 <section className='btns flex'>
                     <div className='share-btn flex'>
                         <SvgHandler svgName={SHARE} />
                         <span>Share</span>
+                        <div className="share-btn-overlay"></div>
                     </div>
                     <div className='save-btn flex' onClick={() => setIsHeartClicked(prevHeart => !prevHeart)}>
                         <SvgHandler svgName={heartSvg} />
                         <span>Save</span>
+                        <div className="save-btn-overlay"></div>
                     </div>
                 </section>
             </section>
@@ -134,8 +137,8 @@ export function StayDetails() {
             <section className='stay-review-details'>
                 <section className='about-host flex space-between'>
                     <section className='host-info'>
-                        <h2>Entire villa hosted by Joan Felip</h2>
-                        <span>4 • guests1 • bedroom2 • beds1 • bath</span>
+                        <h2>Entire villa hosted by {stay.owner ? stay.owner : 'Juan'}</h2>
+                        <span>4 guests • 1 bedroom • 2 beds • 1 bath</span>
                     </section>
                     <img src="https://a0.muscache.com/im/pictures/user/59da4e65-e5a0-4fde-b4d9-e48f20c1ba43.jpg?im_w=240" alt="host image" />
                 </section>
@@ -185,31 +188,75 @@ export function StayDetails() {
             </section>
         </section>
         {stay.reviews.length > 0 &&
-                    <section className="review-container">
-                        <div className="review-title"><SvgHandler svgName={STAR} />
-                            {reviewService.getAverageReview(stay)} • {stay.reviews.length} {reviews}
-                        </div>
-                        <div className="reviews-inputs">
-                            {Object.entries(reviewsInputs).map(([key, value]) => (
-                                <div className="review-input" key={key}>
-                                    <div className="review-input-key">{key}</div>
-                                    <div className="progress-bar-container">
-                                        <div className="review-input-bar" style={{ width: `${calculatePercentage(value)}%` }}><span>{value.toFixed(1)}</span>
-                                        </div>
-                                    </div>
+            <section className="review-container">
+                <div className="review-title"><SvgHandler svgName={STAR} />
+                    {reviewService.getAverageReview(stay)} • {stay.reviews.length} {reviews}
+                </div>
+                <div className="reviews-inputs">
+                    {Object.entries(reviewsInputs).map(([key, value]) => (
+                        <div className="review-input" key={key}>
+                            <div className="review-input-key">{key}</div>
+                            <div className="progress-bar-container">
+                                <div className="review-input-bar" style={{ width: `${calculatePercentage(value)}%` }}><span>{value.toFixed(1)}</span>
                                 </div>
-                            ))}
+                            </div>
                         </div>
-                        {displayReviews()}
-                        {stay.reviews.length > 6 && <button>Show all {stay.reviews.length} reviews</button>}
-                    </section>}
-                <section className="map-container">
-                    <h3>Where you'll be</h3>
-                    {stay.loc.city}, {stay.loc.country}
-                </section>
-
-                <section className="host-details-container">
-
-                </section>
+                    ))}
+                </div>
+                {displayReviews()}
+                {stay.reviews.length > 6 && <button>Show all {stay.reviews.length} reviews</button>}
+            </section>}
+        <section className="map-container">
+            <h3>Where you'll be</h3>
+            {stay.loc.city}, {stay.loc.country}
+        </section>
+        <section className="host-details-container">
+            <div className="name-details">
+                <img src="https://a0.muscache.com/im/pictures/user/59da4e65-e5a0-4fde-b4d9-e48f20c1ba43.jpg?im_w=240" alt="host image" />
+                Hosted by {stay.owner ? stay.owner : 'Juan'}
+                <p>Joined in March 2014</p>
+            </div>
+            <div className="sub-details">
+                <SvgHandler svgName={STAR} />
+                {stay.reviews.length} {capitalizedReviewsString}
+                <SvgHandler svgName={STAR} />
+                Identity verified
+                {stay.superhost && (
+                    <>
+                        <SvgHandler svgName={STAR} />
+                        Superhost
+                    </>
+                )}
+            </div>
+            {stay.superhost && (
+                <>
+                    <p>{stay.owner} is a Superhost</p>
+                    <p>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
+                </>
+            )}
+            Language: English
+            Response rate: 100%
+            Response time: within a couple of hours
+            <button>Contact Host</button>
+        </section>
+        <section className="things-to-know">
+            <div className="house-rules">
+                <p>House rules</p>
+                <p>Check-in after 4:00 PM</p>
+                <p>Checkout before 10:00 AM</p>
+                <p>Pets allowed</p>
+            </div>
+            <div className="house-rules">
+                <p>Safety & property</p>
+                <p>No smoke alarm</p>
+                <p>Pool/hot tub without a gate or lock</p>
+                <p>Carbon monoxide detector not required</p>
+            </div>
+            <div className="house-rules">
+                <p>Cancellation policy</p>
+                <p>Free cancellation before {stay.checkIn}</p>
+                <p>Review the Host's full cancellation policy which applies even if you cancel for illness for disruptions caused by COVID-19</p>
+            </div>
+        </section>
     </section >
 }
