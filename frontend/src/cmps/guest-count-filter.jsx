6e4a2e48ac7857c256c77a74afe97787efc1894e
Steveb599 <Steveb599@gmail.com>
2023-06-03 20:05:40 +0300

    disabled buttons when zero on the search

--- frontend/src/cmps/guest-count-filter.jsx
+++ frontend/src/cmps/guest-count-filter.jsx
@@ -1,5 +1,16 @@
-export function GuestCountFilter() {
+import { useState } from "react"
 
+export function GuestCountFilter() {
+    const [guestCounts, setGuestCounts] = useState({
+        adults: 0,
+        children: 0,
+        infants: 0,
+        pets: 0,
+    })
+
+    function handleGuestCountChange(type, value) {
+        setGuestCounts((prevGuestCounts) => ({ ...prevGuestCounts, [type]: prevGuestCounts[type] + value }))
+    }
     return (
         <section className="guest-count-filter">
             <article className="card">
@@ -8,9 +19,9 @@ export function GuestCountFilter() {
                     <p>Ages 13 or above</p>
                 </section>
                 <section>
-                    <button className="custom-btn-guest-count">-</button>
-                    <span>0</span>
-                    <button className="custom-btn-guest-count">+</button>
+                    <button className="custom-btn-guest-count" onClick={() => handleGuestCountChange('adults', -1)} disabled={guestCounts.adults === 0}>-</button>
+                    <span>{guestCounts.adults}</span>
+                    <button className="custom-btn-guest-count" onClick={() => handleGuestCountChange('adults', 1)}>+</button>
                 </section>
             </article>
 
@@ -20,9 +31,9 @@ export function GuestCountFilter() {
                     <p>Ages 2–12</p>
                 </section>
                 <section>
-                    <button className="custom-btn-guest-count">-</button>
-                    <span>0</span>
-                    <button className="custom-btn-guest-count">+</button>
+                    <button className="custom-btn-guest-count" onClick={() => handleGuestCountChange('children', -1)} disabled={guestCounts.children === 0}>-</button>
+                    <span>{guestCounts.children}</span>
+                    <button className="custom-btn-guest-count" onClick={() => handleGuestCountChange('children', 1)}>+</button>
                 </section>
             </article>
 
@@ -32,9 +43,9 @@ export function GuestCountFilter() {
                     <p>Under 2</p>
                 </section>
                 <section>
-                    <button className="custom-btn-guest-count">-</button>
-                    <span>0</span>
-                    <button className="custom-btn-guest-count">+</button>
+                    <button className="custom-btn-guest-count" onClick={() => handleGuestCountChange('infants', -1)} disabled={guestCounts.infants === 0}>-</button>
+                    <span>{guestCounts.infants}</span>
+                    <button className="custom-btn-guest-count" onClick={() => handleGuestCountChange('infants', 1)}>+</button>
                 </section>
             </article>
 
@@ -44,9 +55,9 @@ export function GuestCountFilter() {
                     <p>Bringing a service animal?</p>
                 </section>
                 <section>
-                    <button className="custom-btn-guest-count">-</button>
-                    <span>0</span>
-                    <button className="custom-btn-guest-count">+</button>
+                    <button className="custom-btn-guest-count" onClick={() => handleGuestCountChange('pets', -1)} disabled={guestCounts.pets === 0}>-</button>
+                    <span>{guestCounts.pets}</span>
+                    <button className="custom-btn-guest-count" onClick={() => handleGuestCountChange('pets', 1)}>+</button>
                 </section>
             </article>
         </section>
