a0f15a9944b23ebb8d1db0c93115facba0bd4e88
Steveb599 <Steveb599@gmail.com>
2023-06-03 19:34:01 +0300

    merge issues

--- frontend/src/cmps/filter.jsx
+++ frontend/src/cmps/filter.jsx
@@ -25,99 +25,100 @@ export function FilterModal({ stays, setIsFilterModalOpen }) {
             })
             return amenities;
         }, [])
+    }
 
 
-        // const elInputRef = useRef(null)
-        // useEffect(() => {
-        //     elInputRef.current.focus()
-        // }, [])
+    // const elInputRef = useRef(null)
+    // useEffect(() => {
+    //     elInputRef.current.focus()
+    // }, [])
 
-        // useEffect(() => {
-        //     onSetFilter(filterByToEdit)
-        // }, [filterByToEdit])
+    // useEffect(() => {
+    //     onSetFilter(filterByToEdit)
+    // }, [filterByToEdit])
 
-        function handleChange({ target }) {
-            const { name: field, type, checked } = target
-            const value = (type === 'number') ? (+target.value || '') :
-                (type === 'checkbox') ? checked :
-                    target.value
-            setFilterByToSend((prevFilter) => ({ ...prevFilter, [field]: value }))
-        }
+    function handleChange({ target }) {
+        const { name: field, type, checked } = target
+        const value = (type === 'number') ? (+target.value || '') :
+            (type === 'checkbox') ? checked :
+                target.value
+        setFilterByToSend((prevFilter) => ({ ...prevFilter, [field]: value }))
+    }
 
-        // function onLabelChange(selectedLabels) {
-        //     setFilterByToEdit((prevFilter) => ({
-        //         ...prevFilter,
-        //         labels: selectedLabels,
-        //     }))
-        // }
+    // function onLabelChange(selectedLabels) {
+    //     setFilterByToEdit((prevFilter) => ({
+    //         ...prevFilter,
+    //         labels: selectedLabels,
+    //     }))
+    // }
 
 
-        function handleSliderChange(values) {
-            setPriceRange({ minPrice: values[0], maxPrice: values[1] })
-        }
+    function handleSliderChange(values) {
+        setPriceRange({ minPrice: values[0], maxPrice: values[1] })
+    }
 
-        function handleInputChange(ev) {
-            ev.preventDefault();
-            const { name, value } = ev.target;
-            setPriceRange((prevPriceRange) => ({
-                ...prevPriceRange,
-                [name]: Number(value),
-            }));
-        }
+    function handleInputChange(ev) {
+        ev.preventDefault();
+        const { name, value } = ev.target;
+        setPriceRange((prevPriceRange) => ({
+            ...prevPriceRange,
+            [name]: Number(value),
+        }));
+    }
 
-        function onSubmitFilter(ev) {
-            ev.preventDefault()
-            setIsFilterModalOpen(false)
-            updateFilterBy(filterByToSend)
-        }
-        return (
-            <section>
-                <div className="header-div">
-                    <button onClick={() => setIsFilterModalOpen(false)}>X</button>
-                    Filters
+    function onSubmitFilter(ev) {
+        ev.preventDefault()
+        setIsFilterModalOpen(false)
+        updateFilterBy(filterByToSend)
+    }
+    return (
+        <section>
+            <div className="header-div">
+                <button onClick={() => setIsFilterModalOpen(false)}>X</button>
+                Filters
+            </div>
+            <form onSubmit={onSubmitFilter}>
+                <h3>Type of place</h3>
+                <div className="places-type">
+                    <button type="button">
+                        <div className="type">
+                            All types
+                            <span>2,140$ avg.</span>
+                        </div>
+                    </button>
+                    <button type="button">
+                        <div className="type">
+                            Rooms
+                            <span>914$ avg.</span>
+                        </div>
+                    </button >
+                    <button type="button">
+                        <div className="type">
+                            Homes
+                            <span>2357$ avg.</span>
+                        </div>
+                    </button>
                 </div>
-                <form onSubmit={onSubmitFilter}>
-                    <h3>Type of place</h3>
-                    <div className="places-type">
-                        <button type="button">
-                            <div className="type">
-                                All types
-                                <span>2,140$ avg.</span>
-                            </div>
-                        </button>
-                        <button type="button">
-                            <div className="type">
-                                Rooms
-                                <span>914$ avg.</span>
-                            </div>
-                        </button >
-                        <button type="button">
-                            <div className="type">
-                                Homes
-                                <span>2357$ avg.</span>
-                            </div>
-                        </button>
+                <p>Browse rooms, homes and more. Average nightly prices include fees and taxes.</p>
+                <h3>Price Range</h3>
+                <Slider range allowCross={false}
+                    min={initialPriceRange.minPrice}
+                    max={initialPriceRange.maxPrice}
+                    value={[priceRange.minPrice, priceRange.maxPrice]}
+                    onChange={handleSliderChange}
+                    keyboard={true} />
+                <div className="price-ranges">
+                    <div className="price-input">
+                        <label >Minimum:</label>
+                        <input type="number" name="minPrice" value={priceRange.minPrice} onChange={handleInputChange} />
                     </div>
-                    <p>Browse rooms, homes and more. Average nightly prices include fees and taxes.</p>
-                    <h3>Price Range</h3>
-                    <Slider range allowCross={false}
-                        min={initialPriceRange.minPrice}
-                        max={initialPriceRange.maxPrice}
-                        value={[priceRange.minPrice, priceRange.maxPrice]}
-                        onChange={handleSliderChange}
-                        keyboard={true} />
-                    <div className="price-ranges">
-                        <div className="price-input">
-                            <label >Minimum:</label>
-                            <input type="number" name="minPrice" value={priceRange.minPrice} onChange={handleInputChange} />
-                        </div>
-                        <div className="space">--</div>
-                        <div className="price-input">
-                            <label >Maximum:</label>
-                            <input type="number" name="maxPrice" value={priceRange.maxPrice} onChange={handleInputChange} />
-                        </div>
+                    <div className="space">--</div>
+                    <div className="price-input">
+                        <label >Maximum:</label>
+                        <input type="number" name="maxPrice" value={priceRange.maxPrice} onChange={handleInputChange} />
                     </div>
-                    {/* <div className="amenities-container">
+                </div>
+                {/* <div className="amenities-container">
                         <h2>Amenities</h2>
                         <h4>Essentials</h4>
                         {allAmenities.map((amenity) => (
@@ -131,9 +132,9 @@ export function FilterModal({ stays, setIsFilterModalOpen }) {
                                 {amenity}
                             </label>
                         ))} */}
-                </div>
+                {/* </div> */}
             </form>
 
-            </section >
-        )
-    }
+        </section >
+    )
+}
