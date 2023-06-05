afec701320fba96e94c9eeb0c94a9dc0cc321e6e
Vlad Epov <vladep622@gmail.com>
2023-06-05 13:24:09 +0300

    footer random f char thrown fix

--- frontend/src/cmps/app-footer.jsx
+++ frontend/src/cmps/app-footer.jsx
@@ -28,7 +28,7 @@ export function AppFooter() {
             <p className="flex align-center">
 
                 {/* ©{new Date().getFullYear()} StayHub,Inc. · Terms · Sitemap · Privacy · Your Privacy Choices {<svg width="26" height="12" fill="none"><rect x="0.5" y="0.5" width="25" height="11" rx="5.5" fill="#fff"></rect><path d="M14 1h7a5 5 0 010 10H11l3-10z" fill="#06F"></path><path d="M4.5 6.5l1.774 1.774a.25.25 0 00.39-.049L9.5 3.5" stroke="#06F" stroke-linecap="round"></path><path d="M16.5 3.5L19 6m0 0l2.5 2.5M19 6l2.5-2.5M19 6l-2.5 2.5" stroke="#fff" stroke-linecap="round"></path><rect x="0.5" y="0.5" width="25" height="11" rx="5.5" stroke="#06F"></rect></svg>} */}
-                ©{new Date().getFullYear()} StayHub,Inc. · Terms · Sitemap · Privacy · Your Privacy Choices {<SvgHandler svgName={TICK_DECLINE} />}
+                ©{new Date().getFullYear()} StayHub,Inc. · Terms · Sitemap · Privacy · Your Privacy Choices { <SvgHandler svgName={TICK_DECLINE} />}
             </p>
             {cart.length > 0 &&
                 <h5>
