73f129a036840be653b63adc15f36923e6965b0c
GideonLevinson <gideonlevinson@gmail.com>
2023-06-12 00:50:23 +0300

    merge

--- frontend/src/pages/trips.jsx
+++ frontend/src/pages/trips.jsx
@@ -40,56 +40,63 @@ export function MyTrips() {
     const currentDate = new Date();
 
     const upcomingTrips = trips
-        .filter((trip) => new Date(trip.stayDetails.checkOut) > currentDate)
+        .filter((trip) => new Date(trip.checkOut) > currentDate)
         .reverse();
 
     const pastTrips = trips
-        .filter((trip) => new Date(trip.stayDetails.checkOut) <= currentDate)
+        .filter((trip) => new Date(trip.checkOut) <= currentDate)
         .reverse();
-
+    console.log(upcomingTrips)
     return (
         <div className="trips">
             <h1>Trips</h1>
             {trips && trips.length > 0 ? (
                 <section className="trips-container">
                     <h3 className="reservations-header">Upcoming reservations</h3>
-                    <div className="upcoming-reservation">
-                        <section className="reservation-container">
+                        <section className="reservations-container">
                             {upcomingTrips.map((trip, index) => {
-                                const locationSubstring = trip.stayDetails.loc.substring(0, trip.stayDetails.loc.indexOf(','));
+                                const locationSubstring = trip.stayDetails.loc.address.substring(0, trip.stayDetails.loc.address.indexOf(','));
                                 return (
-                                    <div key={index}>
-                                        {trip.stayDetails.city}
-                                        Entire rental unit hosted by {trip.seller.fullname}
-                                        <div className="inside-upcoming-reservation">
-                                            {trip.checkIn} - {trip.checkOut}
-                                            {locationSubstring}
-                                            {trip.stayDetails.loc.city}
-                                            {trip.stayDetails.loc.country}
-                                        </div>
-                                        <div className="upcoming-reservation-picture">{trip.stayDetails.image}</div>
+                                    <div className="upcoming-reservation" key={index}>
+                                        <section className='reservation-container flex'>
+                                            <section className='reservation-info'>
+                                                <section className="reservation-header">
+                                                    <h2> {trip.stayDetails.loc.city}</h2>
+                                                    <h5> Entire rental unit hosted by {trip.seller.fullname}</h5>
+                                                </section>
+                                                <section className="reservation-dates">
+                                                    {utilService.getFormattedTimeRange(trip.checkIn, trip.checkOut)}
+                                                </section>
+                                                <section className="reservation-destination">
+                                                    {locationSubstring}, {trip.stayDetails.loc.city}, {trip.stayDetails.loc.country}
+                                                </section>
+                                            </section>
+                                            <div className="reservation-img">
+                                                <img src={trip.stayDetails.image} alt="Stay Image" />
+                                            </div>
+                                        </section>
                                         <aside className="explore-things-to-do">
                                             {/* Explore things to do */}
                                             <h5>Explore things to do near {trip.stayDetails.city}</h5>
                                             <div className="things-to-do">
                                                 {/* Random experiences */}
                                                 <div className="things-todo">
-                                                    Just for you {utilService.getRandomIntInclusive(10, 20)} experiences
+                                                    Just for you {trip.thingsToDo['Just-for-you']} experiences
                                                 </div>
                                                 <div className="things-todo">
-                                                    Top-rated {utilService.getRandomIntInclusive(30, 50)} experiences
+                                                    Top-rated {trip.thingsToDo['Top-rated']} experiences
                                                 </div>
                                                 <div className="things-todo">
-                                                    Sports {utilService.getRandomIntInclusive(30, 60)} experiences
+                                                    Sports {trip.thingsToDo['Sports']} experiences
                                                 </div>
                                                 <div className="things-todo">
-                                                    Tours {utilService.getRandomIntInclusive(50, 120)} experiences
+                                                    Tours {trip.thingsToDo['Tours']} experiences
                                                 </div>
                                                 <div className="things-todo">
-                                                    Sightseeing {utilService.getRandomIntInclusive(50, 120)} experiences
+                                                    Sightseeing {trip.thingsToDo['Sightseeing']} experiences
                                                 </div>
                                                 <div className="things-todo">
-                                                    Show more {utilService.getRandomIntInclusive(300, 500)} experiences
+                                                    Show more {trip.thingsToDo['more']} experiences
                                                 </div>
                                             </div>
                                         </aside>
@@ -97,7 +104,7 @@ export function MyTrips() {
                                 );
                             })}
                         </section>
-                    </div>
+                   
                     {pastTrips.length > 0 && (
                         <section className="where-you-been">
                             <h3>Where you've been</h3>
@@ -113,7 +120,9 @@ export function MyTrips() {
                                             {trip.stayDetails.loc.city}
                                             {trip.stayDetails.loc.country}
                                         </div>
-                                        <div className="past-trip-picture">{trip.stayDetails.image}</div>
+                                        <div className="past-trip-picture">
+                                            <img src={trip.stayDetails.image} alt="Stay Image" />
+                                        </div>
                                     </div>
                                 );
                             })}
