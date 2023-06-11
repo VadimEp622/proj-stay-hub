8ff4f5d08a339ca5e16a37fe0e3f1209a1fff887
Steveb599 <Steveb599@gmail.com>
2023-06-11 10:49:53 +0300

    added kilometers away

--- frontend/src/cmps/stay-preview.jsx
+++ frontend/src/cmps/stay-preview.jsx
@@ -2,31 +2,25 @@ import { useState, useEffect } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { useSelector } from "react-redux"
 
-
-// import star from '../assets/img/star.svg'
-
-
 import { utilService } from "../services/util.service.js"
 import { reviewService } from "../services/review.service.js"
 import { HEART, STAR, WHITE_HEART } from "../services/svg.service.js"
 
-
 import { setModal } from "../store/stay.actions.js"
 import { AddToWishlist, removeFromWishlist } from "../store/user.actions.js"
-
 
 import SvgHandler from "./svg-handler.jsx"
 import { PreviewImageCarousel } from "./preview-image-carousel.jsx"
 
-
 export function StayPreview({ stay }) {
-
     const [isLikeClicked, setIsLikeClicked] = useState(false)
     const likeSVG = isLikeClicked ? 'heart-red' : 'heart-white'
     const loggedInUser = useSelector(storeState => storeState.userModule.user)
     const wishedListItems = useSelector(storeState => storeState.userModule.user?.wishlist)
     const location = useLocation()
     const isWishlistPage = location.pathname.includes('/wishlist')
+    const [lat, setLat] = useState(null)
+    const [lng, setLng] = useState(null)
 
 
     function onLikeClicked(ev) {
@@ -52,6 +46,38 @@ export function StayPreview({ stay }) {
 
     const bedrooms = stay.bedrooms > 1 ? 'bedrooms' : 'bedroom'
 
+    if ("geolocation" in navigator) {
+        navigator.geolocation.watchPosition(
+            function (position) {
+                setLat(position.coords.latitude)
+                setLng(position.coords.longitude)
+            },
+            function (error) {
+                console.error("Error getting user location:", error)
+            }
+        )
+    }
+
+    function calcCrow(lat1, lon1, lat2, lon2) {
+        var R = 6371 // km
+        var dLat = toRad(lat2 - lat1)
+        var dLon = toRad(lon2 - lon1)
+        lat1 = toRad(lat1)
+        lat2 = toRad(lat2)
+
+        var a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2)
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+        var d = R * c
+        return d
+    }
+
+    // Converts numeric degrees to radians
+    function toRad(Value) {
+        return Value * Math.PI / 180
+    }
+
     return (
         <section className="stay-preview" key={stay._id}>
             <div className="img-container">
@@ -67,7 +93,7 @@ export function StayPreview({ stay }) {
                         <p className="review-rate"><SvgHandler svgName={STAR} /><span>{reviewService.getAverageReview(stay)}</span></p>
                     </div>
                     <div className="stay-info">
-                        {!isWishlistPage ? <p>{stay.type}</p> : <p>{stay.type}</p>}
+                        {!isWishlistPage ? <p>{calcCrow(lat, lng, stay.loc.lat, stay.loc.lan).toFixed(0)} kilometers away</p> : <p>{stay.type}</p>}
                         {isWishlistPage ? <p>{stay.bedrooms} {bedrooms} </p> : ''}
                         <p>{utilService.getFormattedTimeRange(stay.checkIn, stay.checkOut)}</p>
                         <p className="price-preview"><span>${stay.price.toLocaleString()}</span> night</p>
