143166ef651cf0971b3513fec7dce7f85bf8d54c
GideonLevinson <gideonlevinson@gmail.com>
2023-06-13 01:48:02 +0300

    host-orders

--- frontend/src/cmps/host-orders.jsx
+++ frontend/src/cmps/host-orders.jsx
@@ -1,44 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const data = [
+  { guest: 'John Doe', dates: 'Jun 23-27', status: 'Pending' },
+  { guest: 'Jane Smith', dates: 'Mar 11-17', status: 'Approved' },
+  { guest: 'Michael Johnson', dates: 'Feb 15-18', status: 'Rejected' },
+];
 
 export function HostOrders() {
-  const data = [
-    { guest: 'John Doe', dates: 'Jun 23-27', status: 'Pending' },
-    { guest: 'Jane Smith', dates: 'Mar 11-17', status: 'Approved' },
-    { guest: 'Michael Johnson', dates: 'Feb 15-18', status: 'Rejected' },
-  ];
+    const [tableData, setTableData] = useState(data)
 
-  return (
-    <div className="table-container">
-      <table>
-        <thead>
-          <tr>
-            <th>Guest</th>
-            <th>Dates</th>
-            <th>Status</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={index}>
-              <td>{row.guest}</td>
-              <td>{row.dates}</td>
-              <td>{row.status}</td>
-              <td>
-                {row.status === 'Pending' && (
-                  <button >Approve</button> //onClick={() => handleApprove(index)}
-                )}
-              </td>
-              <td>
-                {row.status === 'Pending' && (
-                  <button >Reject</button>  //onClick={() => handleReject(index)}
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
-}
+    const handleApprove = (index) => {
+      const updatedData = [...tableData]
+      updatedData[index].status = 'Approved'
+      setTableData(updatedData)
+    };
+  
+    const handleReject = (index) => {
+      const updatedData = [...tableData]
+      updatedData[index].status = 'Rejected'
+      setTableData(updatedData)
+    };
+  
+    return (
+      <div className="table-wrapper">
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Guest</th>
+                <th>Dates</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.guest}</td>
+                  <td>{item.dates}</td>
+                  <td>
+                    {item.status === 'Pending' ? (
+                      <div className="actions">
+                        <button className="action-button approve" onClick={() => handleApprove(index)}>
+                          Approve
+                        </button>
+                        <button className="action-button reject" onClick={() => handleReject(index)}>
+                          Reject
+                        </button>
+                      </div>
+                    ) : (
+                      <div className="selection">{item.status}</div>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
