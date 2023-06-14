e57a4cf688dc903f69485cb571631dafd67fc407
GideonLevinson <gideonlevinson@gmail.com>
2023-06-14 19:48:18 +0300

    update host-order guests array and some css

--- frontend/src/cmps/host-orders.jsx
+++ frontend/src/cmps/host-orders.jsx
@@ -8,9 +8,9 @@ import SvgHandler from './svg-handler'
 import { STAR } from '../services/svg.service'
 
 const data = [
-  { guest: 'John Doe', imgUrl: userService.randomHostImg(), join: 'Mar 2023', dates: 'Jun 23-27', status: 'Pending' },
-  { guest: 'Jane Smith', imgUrl: userService.randomHostImg(), join: 'Jun 2015', dates: 'Mar 11-17', status: 'Approved' },
-  { guest: 'Michael Johnson', imgUrl: userService.randomHostImg(), join: 'Nov 2020', dates: 'Feb 15-18', status: 'Rejected' },
+  { guest: 'John Doe', imgUrl: 'https://a0.muscache.com/im/pictures/user/74afa56b-cc05-4701-a2ce-f4de5e435504.jpg?im_w=240', join: 'Mar 2023', dates: 'Jun 23-27', status: 'Pending' },
+  { guest: 'Jane Smith', imgUrl: 'https://a0.muscache.com/im/pictures/user/596bca9e-ef37-4f41-b41c-a0904c47ca93.jpg?im_w=240', join: 'Jun 2015', dates: 'Mar 11-17', status: 'Approved' },
+  { guest: 'Michael Johnson', imgUrl: 'https://a0.muscache.com/im/pictures/user/cee1b931-2952-4fc0-9b78-3fb70324c397.jpg?im_w=240', join: 'Nov 2020', dates: 'Feb 15-18', status: 'Rejected' },
 ];
 
 export function HostOrders() {
