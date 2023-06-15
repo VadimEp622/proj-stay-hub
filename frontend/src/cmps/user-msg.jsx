9d698db4be6f86f5cfca063a89dcfd2d321776bf
GideonLevinson <gideonlevinson@gmail.com>
2023-06-15 12:13:22 +0300

    user-msg 3500ms

--- frontend/src/cmps/user-msg.jsx
+++ frontend/src/cmps/user-msg.jsx
@@ -15,7 +15,7 @@ export function UserMsg() {
         timeoutIdRef.current = null
         clearTimeout(timeoutIdRef.current)
       }
-      timeoutIdRef.current = setTimeout(closeMsg, 500000)
+      timeoutIdRef.current = setTimeout(closeMsg, 3500)
     })
 
     socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, (review) => {
