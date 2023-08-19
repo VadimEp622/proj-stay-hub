6864ac93981a109c168f85d8663cd4ea52078392
Vlad Epov <vladep622@gmail.com>
2023-08-19 17:01:19 +0300

    made a custom hook for the main-button, and fixed stay-details, alt-header, reserve button width

--- frontend/src/cmps/_reuseable-cmps/button-main.jsx
+++ frontend/src/cmps/_reuseable-cmps/button-main.jsx
@@ -1,40 +1,10 @@
-import { useEffect, useState } from 'react'
+import React from 'react'
 import { utilService } from '../../services/util.service.js'
+import { useButtonBackgroundStyle } from '../../customHooks/useButtonMainStyle.js'
+
 
 export function ButtonMain({ text, onClickButton = (ev) => { } }) {
-    const [positionX, setPositionX] = useState(0)
-    const [positionY, setPositionY] = useState(0) // Updated initial value
-    const [backgroundStyle, setBackgroundStyle] = useState({})
-
-    useEffect(() => {
-        console.log('(x,y)', `(${positionX}, ${positionY})`)
-
-        // Calculate the position as a fraction of the container's size
-        const fractionX = positionX / 10
-        const fractionY = positionY / 10 // Adjusted for 10 rows
-
-        // Convert the fractions into percentages
-        const percentageX = fractionX * 100
-        const percentageY = fractionY * 100
-
-        const newBackgroundStyle = {
-            backgroundImage: `radial-gradient(
-            circle at ${percentageX}% ${percentageY}%,
-            rgb(241, 37, 75) 0%,
-            rgb(230, 30, 77) 27.5%,
-            rgb(227, 28, 95) 40%,
-            rgb(215, 4, 102) 57.5%,
-            rgb(189, 30, 89) 75%,
-            rgb(189, 30, 89) 100%
-        )`,
-        }
-        setBackgroundStyle(newBackgroundStyle)
-    }, [positionX, positionY])
-
-    const handleCellHover = (x, y) => {
-        setPositionX(x)
-        setPositionY(y)
-    }
+    const { handleCellHover, backgroundStyle } = useButtonBackgroundStyle()
 
     return (
         <section className="btn-main-container">
---
+++ frontend/src/customHooks/useButtonMainStyle.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+
+export function useButtonBackgroundStyle() {
+    const [positionX, setPositionX] = useState(0)
+    const [positionY, setPositionY] = useState(0)
+    const [backgroundStyle, setBackgroundStyle] = useState({})
+
+    useEffect(() => {
+        // Calculate the position as a fraction of the container's size
+        const fractionX = positionX / 10
+        const fractionY = positionY / 10 // Adjusted for 10 rows
+
+        // Convert the fractions into percentages
+        const percentageX = fractionX * 100
+        const percentageY = fractionY * 100
+
+        const newBackgroundStyle = {
+            backgroundImage: `radial-gradient(
+                circle at ${percentageX}% ${percentageY}%,
+                rgb(241, 37, 75) 0%,
+                rgb(230, 30, 77) 27.5%,
+                rgb(227, 28, 95) 40%,
+                rgb(215, 4, 102) 57.5%,
+                rgb(189, 30, 89) 75%,
+                rgb(189, 30, 89) 100%
+            )`,
+        }
+        setBackgroundStyle(newBackgroundStyle)
+    }, [positionX, positionY])
+
+    const handleCellHover = (x, y) => {
+        setPositionX(x)
+        setPositionY(y)
+    }
+
+    return {
+        handleCellHover,
+        backgroundStyle,
+    }
+}
