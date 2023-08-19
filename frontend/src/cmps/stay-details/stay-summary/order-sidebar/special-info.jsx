d4fb44f99122cc2e73435eb2a2ac54d6fa57ab4f
Vlad Epov <vladep622@gmail.com>
2023-08-19 22:18:28 +0300

    added special-info cmp, inside improved order-sidebar, stay-details

--- frontend/src/cmps/stay-details/stay-summary/order-sidebar/special-info.jsx
+++ frontend/src/cmps/stay-details/stay-summary/order-sidebar/special-info.jsx
@@ -8,7 +8,7 @@ import SvgHandler from "../../../svg-handler.jsx"
 export function SpecialInfo({ stay }) {
 
     return (
-        <section className="order-special-info flex">
+        <section className="special-info flex">
             <article className="info-container">
                 <span className="bold fs16 lh20">Lower price.</span>
                 <span className="fs16 lh20">Your dates are ${(stay.price * 0.4).toFixed(0)} less per night compared to the average nightly rate of the last 60 days.</span>
