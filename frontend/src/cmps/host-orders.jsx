cef455c0bd981d0f5c7417c6d392aea06a24bf7e
Vlad Epov <vladep622@gmail.com>
2023-09-01 20:53:59 +0300

    added some todos

--- frontend/src/cmps/host-orders.jsx
+++ frontend/src/cmps/host-orders.jsx
@@ -9,16 +9,17 @@ import { approveOrder, denyOrder, loadOrders } from '../store/order.actions.js'
 import { utilService } from '../services/util.service.js'
 
 
-// TODO: might have to remake this from scratch
+// TODO: add order list + order preview, and demo data preview
+// TODO: consider renaming hostOrders to userOrders(?)/userHostOrders(?)
 
-const data = [
+const demoData = [
   { guest: 'John Doe', imgUrl: 'https://a0.muscache.com/im/pictures/user/74afa56b-cc05-4701-a2ce-f4de5e435504.jpg?im_w=240', join: 'Mar 2023', dates: 'Jun 23-27', status: 'Pending' },
   { guest: 'Jane Smith', imgUrl: 'https://a0.muscache.com/im/pictures/user/596bca9e-ef37-4f41-b41c-a0904c47ca93.jpg?im_w=240', join: 'Jun 2015', dates: 'Mar 11-17', status: 'Approved' },
   { guest: 'Michael Johnson', imgUrl: 'https://a0.muscache.com/im/pictures/user/cee1b931-2952-4fc0-9b78-3fb70324c397.jpg?im_w=240', join: 'Nov 2020', dates: 'Feb 15-18', status: 'Rejected' },
 ]
 
 export function HostOrders({ loggedInUser }) {
-  const [tableData, setTableData] = useState(data)
+  const [tableData, setTableData] = useState(demoData)
   const allOrders = useSelector(storeState => storeState.orderModule.orders)
 
   useEffect(() => {
@@ -73,9 +74,10 @@ export function HostOrders({ loggedInUser }) {
 
             {allOrders.map((order, index) => (
               <tr key={index}>
-                <td>
 
+                <td>
                   <section className='order-mini-user flex'>
+
                     <img src={order.content.buyer.img} alt="guest" />
 
                     <section className='mini-user-info flex'>
@@ -84,7 +86,6 @@ export function HostOrders({ loggedInUser }) {
                     </section>
 
                   </section>
-
                 </td>
 
                 <td>{utilService.getFormattedTimeRange(order.content.checkIn, order.content.checkOut)}</td>
