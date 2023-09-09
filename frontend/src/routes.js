9ad41e5a33be6d122d7a437f64c1c0068b2d752b
Vlad Epov <vladep622@gmail.com>
2023-09-09 15:19:58 +0300

    organized routes.js into pages/cmps I know (are being used), and those I don't know (not used yet)

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,14 +1,19 @@
+// ===================== Routes I Know =====================
 import { StayIndex } from './pages/stay-index.jsx'
 import { StayDetails } from './pages/stay-details.jsx'
 import { OrderConfirmation } from './pages/order-confirmation.jsx'
-import { UserDetails } from './pages/user-details.jsx'
 import { UserWishlist } from './pages/user-wishlist.jsx'
 import { UserDashboard } from './pages/user-dashboard.jsx'
 import { UserTrips } from './pages/user-trips.jsx'
+// =========================================================
+// ================== Routes I Don't Know ==================
+import { UserDetails } from './pages/user-details.jsx'
 import { AddStay } from './pages/add-stay.jsx'
 import { JSONStringify } from './cmps/stringify.jsx'
+// =========================================================
 
 const routes = [
+    // ===================== Routes I Know =====================
     {
         path: '/',
         component: <StayIndex />,
@@ -25,11 +30,6 @@ const routes = [
         label: 'OrderConfirmation'
     },
     {
-        path: 'user/:id',
-        component: <UserDetails />,
-        label: 'UserDetails'
-    },
-    {
         path: 'wishlist',
         component: <UserWishlist />,
         label: 'UserWishlist'
@@ -40,20 +40,28 @@ const routes = [
         label: 'UserDashboard'
     },
     {
-        path: '/dashboard/stay/add',
-        component: <AddStay />,
-        label: 'AddStay'
-    },
-    {
         path: 'trips',
         component: < UserTrips />,
         label: 'MyTrips'
+    },
+    // =========================================================
+    // ================== Routes I Don't Know ==================
+    {
+        path: 'user/:id',
+        component: <UserDetails />,
+        label: 'UserDetails'
+    },
+    {
+        path: '/dashboard/stay/add',
+        component: <AddStay />,
+        label: 'AddStay'
     },
     {
         path: 'stay/stringify',
         component: < JSONStringify />,
         label: 'JSONStringify'
     }
+    // =========================================================
 ]
 
 export default routes
