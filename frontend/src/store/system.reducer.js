e07f223d52bd81861bb8e50e822e5691cceae031
Vlad Epov <vladep622@gmail.com>
2023-09-09 15:08:28 +0300

    organized system store

--- frontend/src/store/system.reducer.js
+++ frontend/src/store/system.reducer.js
@@ -1,14 +1,12 @@
+// =============== Being Used In Cmps I Know ===============
 export const LOADING_START = 'LOADING_START'
 export const LOADING_DONE = 'LOADING_DONE'
 export const SET_UNCLICKABLE_BG = 'SET_UNCLICKABLE_BG'
 export const REMOVE_UNCLICKABLE_BG = 'REMOVE_UNCLICKABLE_BG'
 export const OPEN_EXPANDED_HEADER = 'OPEN_EXPANDED_HEADER'
 export const CLOSE_EXPANDED_HEADER = 'CLOSE_EXPANDED_HEADER'
-export const OPEN_DROPDOWN_BAR = 'OPEN_DROPDOWN_BAR'
-export const CLOSE_DROPDOWN_BAR = 'CLOSE_DROPDOWN_BAR'
 export const OPEN_EXPANDED_HEADER_MODAL = 'OPEN_EXPANDED_HEADER_MODAL'
 export const CLOSE_EXPANDED_HEADER_MODAL = 'CLOSE_EXPANDED_HEADER_MODAL'
-export const SET_IS_SIGNING_UP = 'SET_IS_SIGNING_UP'
 
 // *********** Main App Modal (centered with gray screen) ***********
 export const CLOSE_APP_MODAL = 'CLOSE_APP_MODAL'
@@ -18,18 +16,22 @@ export const SET_APP_MODAL_SIGNUP = 'SET_APP_MODAL_SIGNUP'
 export const SET_APP_MODAL_SIGNUP_QUICK = 'SET_APP_MODAL_SIGNUP_QUICK'
 export const SET_APP_MODAL_MAIN_FILTER = 'SET_APP_MODAL_MAIN_FILTER'
 // ******************************************************************
+// =========================================================
 
 const initialState = {
+  // =============== Being Used In Cmps I Know ===============
   isLoading: false,
   isUnclickableBg: false,
   isFilterExpanded: false,
   isExpandedModalOpen: false,
-  isSigningUp: false,
   appModal: null// Main App Modal 
+  // =========================================================
 }
 
 export function systemReducer(state = initialState, action = {}) {
   switch (action.type) {
+
+    // =============== Being Used In Cmps I Know ===============
     case LOADING_START:
       return { ...state, isLoading: true }
     case LOADING_DONE:
@@ -43,11 +45,9 @@ export function systemReducer(state = initialState, action = {}) {
     case OPEN_EXPANDED_HEADER:
       return { ...state, isFilterExpanded: true }
     case OPEN_EXPANDED_HEADER_MODAL:
-      return { ...state, isExpandedModalOpen: true }
+     return { ...state, isExpandedModalOpen: true }
     case CLOSE_EXPANDED_HEADER_MODAL:
-      return { ...state, isExpandedModalOpen: false }
-    case SET_IS_SIGNING_UP:
-      return { ...state, isSigningUp: action.isSigningUp }
+     return { ...state, isExpandedModalOpen: false }
 
     // *********** Main App Modal (centered with gray screen) ***********
     case CLOSE_APP_MODAL:
@@ -63,7 +63,8 @@ export function systemReducer(state = initialState, action = {}) {
     case SET_APP_MODAL_MAIN_FILTER:
       return { ...state, appModal: SET_APP_MODAL_MAIN_FILTER }
     // ******************************************************************
-
+    // =========================================================
+    
     default: return state
   }
 }
