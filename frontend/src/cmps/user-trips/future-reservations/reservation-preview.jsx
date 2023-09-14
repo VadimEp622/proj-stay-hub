1c1049e9be05ee941e6dcd49190c40c861060eba
Vlad Epov <vladep622@gmail.com>
2023-09-14 12:22:02 +0300

    started working on explore-list in user-trips

--- frontend/src/cmps/user-trips/future-reservations/reservation-preview.jsx
+++ frontend/src/cmps/user-trips/future-reservations/reservation-preview.jsx
@@ -4,6 +4,7 @@ import { utilService } from "../../../services/util.service.js"
 
 // Components
 import SvgHandler from "../../_reuseable-cmps/svg-handler.jsx"
+import { ExploreList } from "./reservation-preview/explore-list.jsx"
 
 
 export function ReservationPreview({ trip, explore }) {
@@ -49,66 +50,7 @@ export function ReservationPreview({ trip, explore }) {
 
             </section>
 
-            <section className="explore-things-to-do">
-
-                <h5 className="explore-title fs14">Explore things to do near {trip.content.stayDetails.loc.city}</h5>
-
-                <section className="things-to-do">
-
-                    <section className="to-do">
-                        <img src={explore.justForYou[Math.floor(Math.random() * explore.sightseeing.length)]} alt="to-do" />
-                        <article className='todo-info'>
-                            <h4>Just for you</h4>
-                            <span>{trip.content.thingsToDo['Just-for-you']} experiences</span>
-                        </article>
-                    </section>
-
-                    <section className="to-do">
-                        <img src={explore.topRated[Math.floor(Math.random() * explore.sightseeing.length)]} alt="top rated" />
-                        <article className='todo-info'>
-                            <h4>Top-rated</h4>
-                            <span>{trip.content.thingsToDo['Top-rated']} experiences</span>
-                        </article>
-                    </section>
-
-                    <section className="to-do">
-                        <img src={explore.sports[Math.floor(Math.random() * explore.sightseeing.length)]} alt="sports" />
-                        <article className='todo-info'>
-                            <h4>Sports</h4>
-                            <span>{trip.content.thingsToDo['Sports']} experiences</span>
-                        </article>
-                    </section>
-
-                    <section className="to-do">
-                        <img src={explore.tours[Math.floor(Math.random() * explore.sightseeing.length)]} alt="tours" />
-                        <article className='todo-info'>
-                            <h4>Tours</h4>
-                            <span>{trip.content.thingsToDo['Tours']} experiences</span>
-                        </article>
-                    </section>
-
-                    <section className="to-do">
-                        <img src={explore.sightseeing[Math.floor(Math.random() * explore.sightseeing.length)]} alt="sightseeing" />
-                        <article className='todo-info'>
-                            <h4>Sightseeing</h4>
-                            <span>{trip.content.thingsToDo['Sightseeing']} experiences</span>
-                        </article>
-                    </section>
-
-                    <section className="to-do">
-                        <article className="show-more">
-                            <SvgHandler svgName={EYE} />
-                        </article>
-                        <article className='todo-info'>
-                            <h4>Show more</h4>
-                            <span>{trip.content.thingsToDo['more']} experiences</span>
-                        </article>
-                    </section>
-
-                </section>
-
-            </section>
-
+            <ExploreList explore={explore} trip={trip}/>
         </section>
     )
 }
---
+++ frontend/src/cmps/user-trips/future-reservations/reservation-preview/explore-list/explore-preview.jsx
@@ -0,0 +1,12 @@
+export function ExplorePreview() {
+
+    return (
+        <section className="to-do">
+            {/* <img src={explore.justForYou[Math.floor(Math.random() * explore.sightseeing.length)]} alt="to-do" />
+            <article className="todo-info">
+              <h4>Just for you</h4>
+              <span>{trip.content.thingsToDo['Just-for-you']} experiences</span>
+            </article> */}
+        </section>
+    )
+}
