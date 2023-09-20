5be68b9a9734a58b3a09f6774755f5c6ebae5140
Vlad Epov <vladep622@gmail.com>
2023-09-20 16:54:52 +0300

    fixed stay-details alt-header component styling

--- frontend/src/cmps/stay-details/stay-details-alt-header.jsx
+++ frontend/src/cmps/stay-details/stay-details-alt-header.jsx
@@ -1,19 +1,18 @@
 // Node modules
-import { useEffect } from "react"
+import { useEffect } from 'react'
 
 // Services
-import { reviewService } from "../../services/review.service.js"
-import { STAR } from "../../services/svg.service.js"
+import { reviewService } from '../../services/review.service.js'
+import { STAR } from '../../services/svg.service.js'
 
 // Components
-import { ButtonMain } from "../_reuseable-cmps/button-main.jsx"
-import SvgHandler from "../_reuseable-cmps/svg-handler.jsx"
+import { ButtonMain } from '../_reuseable-cmps/button-main.jsx'
+import SvgHandler from '../_reuseable-cmps/svg-handler.jsx'
 
 
 // TODO: check if these observers here need to be unobserved when component unmounts 
 
 export function StayDetailsAltHeader({ stay, selectedRange, onCheckAvailabilityClick, onReserveClick }) {
-    const reviews = stay.reviews.length > 1 ? 'reviews' : 'review'
 
 
     useEffect(() => {
@@ -29,7 +28,7 @@ export function StayDetailsAltHeader({ stay, selectedRange, onCheckAvailabilityC
 
         function onHeaderObserved(entries) {
             entries.forEach((entry) => {
-                nav.style.display = entry.isIntersecting ? 'none' : 'block'
+                nav.style.display = entry.isIntersecting ? 'none' : 'grid'
             })
         }
 
@@ -47,28 +46,36 @@ export function StayDetailsAltHeader({ stay, selectedRange, onCheckAvailabilityC
     }, [])
 
 
+    const reviews = stay.reviews.length > 1 ? 'reviews' : 'review'
     return (
-        <section className='stay-details-alt-header-container'>
+        <section className='stay-details-alt-header-container details-layout'>
+
             <section className='stay-details-alt-header flex space-between'>
-                <section className='links-container flex align-center'>
+
+                <section className='links-container ff-circular-semibold fs14 lh20 flex align-center'>
                     <a className='detail-link' href='#photos'>Photos</a>
                     <a className='detail-link' href='#amenities'>Amenities</a>
                     <a className='detail-link' href='#reviews'>Reviews</a>
                     <a className='detail-link' href='#location'>Location</a>
                 </section>
+
                 <section className='reserve-container'>
                     <section className='reserve-info flex align-center justify-center'>
-                        <section className='reserve-prev flex'>
-                            <section className='flex'>
-                                <p><span>${stay.price.toLocaleString()}</span> night</p>
+                        <section className='reserve-preview flex'>
+
+                            <section className='price flex'>
+                                <span className='ff-circular-semibold fs16 lh20'>${stay.price.toLocaleString()}&nbsp;</span>
+                                <span className='fs14 lh18'>night</span>
                             </section>
-                            <section className='reserve-review flex align-baseline'>
-                                <span className='star'><SvgHandler svgName={STAR} /></span>
-                                <p> <span className="review-rate">{reviewService.getAverageReview(stay)}</span></p>
-                                <span className='period'>·</span>
-                                <span className="review-count">{stay.reviews.length} {reviews}</span>
+
+                            <section className='score ff-circular-semibold flex align-baseline'>
+                                <span className='star fs10 lh16'><SvgHandler svgName={STAR} /></span>
+                                <span className='review-rate fs12 lh16'>{reviewService.getAverageReview(stay)} ·</span>
+                                <span className='review-count fs12 lh16'>{stay.reviews.length} {reviews}</span>
                             </section>
+
                         </section>
+
                         {(selectedRange.from && selectedRange.to)
                             ? <ButtonMain
                                 text={'Reserve'}
