73d39617a8c0375ac4faf8ae7b896c514ca44554
Vlad Epov <vladep622@gmail.com>
2023-09-20 20:29:34 +0300

    added observers disconnect upon component unmount, to prevent issues and memory leaks

--- frontend/src/cmps/stay-details/stay-details-alt-header.jsx
+++ frontend/src/cmps/stay-details/stay-details-alt-header.jsx
@@ -18,32 +18,36 @@ export function StayDetailsAltHeader({ stay, selectedRange, onCheckAvailabilityC
 
 
     useEffect(() => {
-        const header = document.querySelector('.stay-photos-container')
-        const nav = document.querySelector('.stay-details-alt-header-container')
-        const orderModal = document.querySelector('.order-sidebar')
+        const photos = document.querySelector('.stay-photos-container')
+        const altHeader = document.querySelector('.stay-details-alt-header-container')
+        const orderSidebarBtn = document.querySelector('.order-sidebar .btn-main-container')
         const reserveLink = document.querySelector('.reserve-container')
 
-        const headerObserver = new IntersectionObserver(onHeaderObserved, {
+        const photosObserver = new IntersectionObserver(onPhotosObserved, {
             rootMargin: "-5px 0px 0px",
         })
-        headerObserver.observe(header)
+        const orderSidebarBtnObserver = new IntersectionObserver(onOrderSidebarBtnObserved, {
+            rootMargin: "-80px 0px 0px 0px"
+        })
 
-        function onHeaderObserved(entries) {
+        function onPhotosObserved(entries) {
             entries.forEach((entry) => {
-                nav.style.display = entry.isIntersecting ? 'none' : 'grid'
+                altHeader.style.display = entry.isIntersecting ? 'none' : 'grid'
             })
         }
 
-
-        const orderModalObserver = new IntersectionObserver(onOrderModalObserved, {
-            rootMargin: "-500px 0px 0px",
-        })
-        orderModalObserver.observe(orderModal)
-
-        function onOrderModalObserved(entries) {
+        function onOrderSidebarBtnObserved(entries) {
             entries.forEach((entry) => {
                 reserveLink.style.display = entry.isIntersecting ? 'none' : 'block'
             })
+        }
+
+        photosObserver.observe(photos)
+        orderSidebarBtnObserver.observe(orderSidebarBtn)
+
+        return () => {
+            photosObserver.disconnect()
+            orderSidebarBtnObserver.disconnect()
         }
     }, [])
 
