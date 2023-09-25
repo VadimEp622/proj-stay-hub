86c12c0019005188a2218eec39a1e09c29d58ed0
Vlad Epov <vladep622@gmail.com>
2023-09-25 09:27:13 +0300

    restructured things-to-know cmp styling

--- frontend/src/cmps/stay-details/stay-things-to-know.jsx
+++ frontend/src/cmps/stay-details/stay-things-to-know.jsx
@@ -2,23 +2,25 @@ export function ThingsToKnow({ checkIn }) {
 
     return (
 
-        <section className="things-to-know fs16">
-            <h2>Things to know</h2>
-            <section className='rules-container flex'>
-                <div className="house-rules">
+        <section className='things-to-know'>
+            <h2 className='title fs22 lh26'>Things to know</h2>
+            <section className='rules-container flex fs16 lh20'>
+
+                <section className='house-rules'>
                     <h4>House rules</h4>
                     <p>Check-in after 4:00 PM</p>
                     <p>Checkout before 10:00 AM</p>
                     <p>Pets allowed</p>
-                </div>
-                <div className="safety">
+                </section>
+
+                <section className='safety'>
                     <h4>Safety & property</h4>
                     <p>No smoke alarm</p>
                     <p>Pool/hot tub without a gate or lock</p>
                     <p>Carbon monoxide detector not required</p>
-                </div>
+                </section>
 
-                <div className="cancelation">
+                <section className='cancelation'>
                     <h4>Cancellation policy</h4>
                     {checkIn ?
                         <>
@@ -27,7 +29,8 @@ export function ThingsToKnow({ checkIn }) {
                         </>
                         : <p>Add your trip dates to get the cancellation details for this stay. </p>
                     }
-                </div>
+                </section>
+
             </section>
         </section>
     )
