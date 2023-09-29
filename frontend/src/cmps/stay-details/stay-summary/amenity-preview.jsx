0f593ef1e4b5eecec739f5cca8aa5c1bdad56188
Vlad Epov <vladep622@gmail.com>
2023-09-29 19:12:24 +0300

    improved responsiveness of amenity-list cmp, and structured it with approach of mobile-first

--- frontend/src/cmps/stay-details/stay-summary/amenity-preview.jsx
+++ frontend/src/cmps/stay-details/stay-summary/amenity-preview.jsx
@@ -3,7 +3,7 @@ import SvgHandler from '../../_reuseable-cmps/svg-handler'
 export function AmenityPreview({ amenity }) {
 
     return (
-        <section className='amenity-preview fs16 lh20 flex align-center'>
+        <section className='amenity-preview fs16 lh20 flex align-center gap16'>
             <SvgHandler svgName={amenity} />
             <span>{amenity}</span>
         </section>
