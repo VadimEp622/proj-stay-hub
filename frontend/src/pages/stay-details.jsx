cdba114e1aae932f5698c5c15a8a3e7ac3462be1
Vlad Epov <vladep622@gmail.com>
2023-09-29 19:00:48 +0300

    improved stay-overview responsiveness

--- frontend/src/pages/stay-details.jsx
+++ frontend/src/pages/stay-details.jsx
@@ -39,6 +39,8 @@ import useIsMobile from '../customHooks/useIsMobile.js'
 // TODO: at 790px screen width, turn stay-details into mobile
 //     1. stay pictures only has 1 picture full screen width
 //     2. rest of stay-details is at distance of 24px from left/right screen edge
+
+// TODO: AmenityList block responsiveness
 // ---------------------------------------------
 // ---------------------------------------------
 
