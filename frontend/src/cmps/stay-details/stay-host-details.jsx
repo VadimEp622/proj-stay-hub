0e2cceb8cb1c55a959539d0cf179af34a35b9f53
Vlad Epov <vladep622@gmail.com>
2023-09-30 16:56:50 +0300

    added responsiveness for stay-host-details, with a mobile first approach

--- frontend/src/cmps/stay-details/stay-host-details.jsx
+++ frontend/src/cmps/stay-details/stay-host-details.jsx
@@ -6,32 +6,38 @@ import SvgHandler from '../_reuseable-cmps/svg-handler.jsx'
 
 export function HostDetails({ stay, hostImgUrl, randomDateJoined }) {
 
-    const reviews = stay.reviews.length > 1 ? 'reviews' : 'review'
+    const hostName = stay.host.fullname
+    const hostLocation = stay.host.location
+    const isSuperHost = stay.host.isSuperhost
+    const aboutHost = stay.host?.about
+    const reviewsCount = stay.reviews.length
     const responseTime = stay.host?.responseTime ? stay.host.responseTime : 'within a few hours'
+
+    const reviewCountStr = `${reviewsCount} ${reviewsCount > 1 ? 'reviews' : 'review'}`
     return (
         <section className='host-details-container'>
 
-            <section className='host-details-header flex align-center'>
+            <section className='host-details-header flex align-center gap16'>
                 <img src={hostImgUrl} alt='host' />
                 <section className='host-intro-info'>
-                    <h3 className='host-name fs22'>Hosted by {stay.host.fullname}</h3>
+                    <h3 className='host-name fs22'>Hosted by {hostName}</h3>
                     <span className='host-join-date'>Joined in {randomDateJoined}</span>
                 </section>
             </section>
 
-            <section className='host-details-main flex'>
+            <section className='host-details-main'>
 
                 <section className='host-main-info-container'>
-                    <section className='host-tags fs16 lh20 flex align-center'>
+                    <section className='host-tags fs16 lh20 flex align-center flex-wrap'>
                         <section className='reviews-count flex align-center'>
                             <SvgHandler svgName={STAR_16} />
-                            <span>{`${stay.reviews.length} ${reviews}`}</span>
+                            <span>{reviewCountStr}</span>
                         </section>
                         <section className='identity-verified flex align-center'>
                             <SvgHandler svgName={VERIFIED} />
                             <span>identity verified</span>
                         </section>
-                        {stay.host.isSuperhost &&
+                        {isSuperHost &&
                             <section className='super-host flex align-center'>
                                 <SvgHandler svgName={BLACK_SUPERHOST_16} />
                                 <span>superhost</span>
@@ -41,24 +47,26 @@ export function HostDetails({ stay, hostImgUrl, randomDateJoined }) {
 
                     <section className='host-main-info fs16'>
 
-                        {stay.host?.about &&
-                            <section className="host-about">
-                                <p className='lh24'>{stay.host.about}</p>
+                        {aboutHost &&
+                            <section className='host-about'>
+                                <p className='lh24'>{aboutHost}</p>
                             </section>
                         }
 
                         <section className='during-stay'>
                             <h4 className='lh20'>During your stay</h4>
                             <p className='lh24'>
-                                We live permanently in {stay.host.location} with our family and are on site to answer any questions our guests have.
+                                We live permanently in {hostLocation} with our family and are on site to answer any questions our guests have.
                                 We cook for our guests once a week and hold a weekly pizza night.
                             </p>
                         </section>
 
-                        <section className='is-super-host'>
-                            <h4 className='lh20'>{stay.host.fullname} is a Superhost</h4>
-                            <p className='lh24'>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
-                        </section>
+                        {isSuperHost &&
+                            <section className='is-super-host'>
+                                <h4 className='lh20'>{hostName} is a Superhost</h4>
+                                <p className='lh24'>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
+                            </section>
+                        }
 
                     </section>
                 </section>
@@ -70,7 +78,7 @@ export function HostDetails({ stay, hostImgUrl, randomDateJoined }) {
                         <li>Response time: <span>{responseTime}</span></li>
                     </ul>
                     <button className='ff-circular-semibold fs16 lh20'>Contact Host</button>
-                    <section className='protection-info flex align-center fs12'>
+                    <section className='protection-info flex space-between align-center gap16 fs12'>
                         <img src='https://res.cloudinary.com/dnhn4zsy0/image/upload/v1685913828/airbnb-orotect_ohgcnp.svg' alt='airbnb protect' />
                         <span>To protect your payment, never transfer money or communicate outside of the Airbnb website or app.</span>
                     </section>
