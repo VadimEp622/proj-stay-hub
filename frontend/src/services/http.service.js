e9663c8f41a95ce40c3ae405192d42757ec5a428
Vlad Epov <vladep622@gmail.com>
2023-10-13 11:02:27 +0300

    added option to use .env variables in both back/front-end

--- frontend/src/services/http.service.js
+++ frontend/src/services/http.service.js
@@ -4,6 +4,7 @@ const BASE_URL = process.env.NODE_ENV === 'production'
     ? '/api/'
     : '//localhost:3030/api/'
 
+    console.log('process.env', process.env)
 
 var axios = Axios.create({
     withCredentials: true
