7d63c40d7d0e19a592645ca07dacb14e6408dbdf
Vlad Epov <vladep622@gmail.com>
2023-10-16 16:02:57 +0300

    removed message host, at order-confirmation

--- frontend/src/cmps/order-confirmation/confirmation-main.jsx
+++ frontend/src/cmps/order-confirmation/confirmation-main.jsx
@@ -3,8 +3,6 @@ import { utilService } from '../../services/util.service.js'
 
 
 export function ConfirmationMain({ orderObject }) {
-
-    const { fullname, img: sellerPicture, joined } = orderObject.seller
     const { checkIn, checkOut, guestCount } = orderObject.orderDetails
 
     const guestCountStr = `${guestCount} ${guestCount === 1 ? 'guest' : 'guests'}`
@@ -26,26 +24,6 @@ export function ConfirmationMain({ orderObject }) {
                     <h4>Guests</h4>
                     <p>{guestCountStr}</p>
                 </section>
-            </section>
-
-            <section className='message-host'>
-                <section className='intro'>
-                    <h3 className='fs22'>Required for your trip</h3>
-                    <h5 className='fs16'>Message the Host</h5>
-                    <p>Let the Host know why you're traveling and when you'll check in.</p>
-                </section>
-
-                <section className='host-preview flex align-center gap16'>
-                    <section className='img-container flex'>
-                        <img src={sellerPicture} alt='host' />
-                    </section>
-                    <section className='flex column'>
-                        <span className='fs16 ff-circular-semibold'>{fullname}</span>
-                        <span className='capitalize'>joined in {joined}</span>
-                    </section>
-                </section>
-
-                <textarea name='' id='' cols='30' rows='10'></textarea>
             </section>
 
             <section className='cancellation-policy'>
