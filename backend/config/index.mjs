10fa4ae9f3cea69c54b8126643046bef825eb06d
Vlad Epov <vladep622@gmail.com>
2023-11-27 19:10:02 +0200

    1. added backup MongoDB collections for local use 2. restored ability for local development with local database

--- backend/config/index.mjs
+++ backend/config/index.mjs
@@ -6,7 +6,7 @@ dotenv.config()
 
 export var config
 // is needed to be TRUE, so all those who build the project, will run on the same (Atlas - online) Database.
-if (true || process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   config = configProd
 } else {
   config = configDev
--- backend/server.mjs
+++ backend/server.mjs
@@ -20,10 +20,13 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve('public')))
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:3000',
+        origin: [
             'http://localhost:3000',
-            'http://127.0.0.1:5173',
-            'http://localhost:5173'
+            'http://127.0.0.1:3000',
+            'http://localhost:3030',
+            'http://127.0.0.1:3030',
+            'http://localhost:5173',
+            'http://127.0.0.1:5173'
         ],
         credentials: true
     }
