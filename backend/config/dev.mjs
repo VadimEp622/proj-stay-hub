7f519d6f26e5f42fbd5e5400f1d13ae388f0f7be
Vlad Epov <vladep622@gmail.com>
2023-12-10 12:46:21 +0200

    removed DB_URL  from env-variables due to redundancy.

--- backend/config/dev.mjs
+++ backend/config/dev.mjs
@@ -3,5 +3,5 @@ dotenv.config()
 
 export default {
   dbURL: 'mongodb://127.0.0.1:27017',
-  dbName: process.env.DB_NAME
+  dbName: 'StayHub_db'
 }
--- backend/config/prod.mjs
+++ backend/config/prod.mjs
@@ -3,5 +3,5 @@ dotenv.config()
 
 export default {
   dbURL: process.env.DB_URL,
-  dbName: process.env.DB_NAME
+  dbName: 'StayHub_db'
 }
