4530b275021343d27db129b6347a001fb3bc0299
Vlad Epov <vladep622@gmail.com>
2024-02-25 19:16:53 +0200

    added MyCustomRouter with useRoutes, which replaces const route array mapping

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,39 +4,73 @@ import { OrderConfirmation } from './pages/order-confirmation.jsx'
 import { UserWishlist } from './pages/user-wishlist.jsx'
 import { UserDashboard } from './pages/user-dashboard.jsx'
 import { UserTrips } from './pages/user-trips.jsx'
+import { useRoutes } from 'react-router-dom'
 
 
-const routes = [
-    {
-        path: '/',
-        component: <StayIndex />,
-        label: 'StayHub'
-    },
-    {
-        path: 'stay/:stayId',
-        component: <StayDetails />,
-        label: 'StayDetails'
-    },
-    {
-        path: 'stay/book/:stayId',
-        component: <OrderConfirmation />,
-        label: 'OrderConfirmation'
-    },
-    {
-        path: 'wishlist',
-        component: <UserWishlist />,
-        label: 'UserWishlist'
-    },
-    {
-        path: 'dashboard',
-        component: <UserDashboard />,
-        label: 'UserDashboard'
-    },
-    {
-        path: 'trips',
-        component: < UserTrips />,
-        label: 'MyTrips'
-    }
-]
+export default function MyCustomRouter() {
+    return useRoutes([
+        {
+            path: '/',
+            element: <StayIndex />,
+            label: 'StayHub'
+        },
+        {
+            path: 'stay',
+            children: [
+                { path: ':stayId', element: <StayDetails />, label: 'StayDetails' },
+                { path: 'book/:stayId', element: <OrderConfirmation />, label: 'OrderConfirmation' }
+            ]
+        },
+        {
+            path: 'wishlist',
+            element: <UserWishlist />,
+            label: 'UserWishlist'
+        },
+        {
+            path: 'dashboard',
+            element: <UserDashboard />,
+            label: 'UserDashboard'
+        },
+        {
+            path: 'trips',
+            element: <UserTrips />,
+            label: 'MyTrips'
+        }
+    ])
+}
 
-export default routes
+
+// const routes = [
+//     {
+//         path: '/',
+//         component: <StayIndex />,
+//         label: 'StayHub'
+//     },
+//     {
+//         path: 'stay/:stayId',
+//         component: <StayDetails />,
+//         label: 'StayDetails'
+//     },
+//     {
+//         path: 'stay/book/:stayId',
+//         component: <OrderConfirmation />,
+//         label: 'OrderConfirmation'
+//     },
+//     {
+//         path: 'wishlist',
+//         component: <UserWishlist />,
+//         label: 'UserWishlist'
+//     },
+//     {
+//         path: 'dashboard',
+//         component: <UserDashboard />,
+//         label: 'UserDashboard'
+//     },
+//     {
+//         path: 'trips',
+//         component: < UserTrips />,
+//         label: 'MyTrips'
+//     }
+// ]
+
+// export default routes
