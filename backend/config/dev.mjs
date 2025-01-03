3bb9023cc8f7b104e5d2a3b241f6a657d9c0b1a4
Vlad Epov <vladep622@gmail.com>
2025-01-03 13:32:39 +0200

    removed old backend in favor of new

--- backend/config/dev.mjs
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-export default {
-  dbURL: 'mongodb://127.0.0.1:27017',
-  dbName: 'StayHub_db'
-}
--- backend/middlewares/logger.middleware.mjs
+++
@@ -1,7 +0,0 @@
-import {logger} from '../services/logger.service.mjs'
-
-export async function log(req, res, next) {
-  logger.info(req.method, req.originalUrl)
-  next()
-}
-
