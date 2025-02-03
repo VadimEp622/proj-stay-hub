9dd256076c05ab4ea5a5e2716ea55fb5ef87bc58
Vlad Epov <vladep622@gmail.com>
2025-02-03 15:36:43 +0200

    updated notes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -64,7 +64,7 @@ import { connectDB } from "./service/db.service.ts";
 //     (basically, every time user clicks on header's red search button, category resets to "all")
 //   3) ✔ in stay-index, make clicking on a category update the URL query (which should trigger a fresh filtered stay list with infinite scroll)
 //   4) ✔ combine redux store's stay slice's "loadStays" and "loadMoreStays" async thunk actions into one.
-//   5) combine redux store's stay slice's "loadWishlistedStayId" and "loadWishlistedStayIds" async thunk actions into one.
+//   5) ✔ combine redux store's stay slice's "loadWishlistedStayId" and "loadWishlistedStayIds" async thunk actions into one.
 
 // ####################################################################################################################
 // ####################################################################################################################
